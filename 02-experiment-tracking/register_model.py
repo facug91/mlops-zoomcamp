02b18945f0d4381f1de1b20cd631885caef8b3ad
Facundo Galan <facundogalan91@gmail.com>
2025-05-29 00:13:33 -0300

    Update 02-experiment-tracking

--- 02-experiment-tracking/register_model.py
+++ 02-experiment-tracking/register_model.py
@@ -36,9 +36,9 @@ def train_and_log_model(data_path, params):
         rf.fit(X_train, y_train)
 
         # Evaluate model on the validation and test sets
-        val_rmse = mean_squared_error(y_val, rf.predict(X_val), squared=False)
+        val_rmse = mean_squared_error(y_val, rf.predict(X_val)) ** 0.5
         mlflow.log_metric("val_rmse", val_rmse)
-        test_rmse = mean_squared_error(y_test, rf.predict(X_test), squared=False)
+        test_rmse = mean_squared_error(y_test, rf.predict(X_test)) ** 0.5
         mlflow.log_metric("test_rmse", test_rmse)
 
 
@@ -72,9 +72,20 @@ def run_register_model(data_path: str, top_n: int):
     # Select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
     # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs(
+        experiment_ids=experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=1,
+        order_by=["metrics.rmse ASC"]
+    )[0]
 
     # Register the best model
     # mlflow.register_model( ... )
+    run_id = best_run.info.run_id
+    mlflow.register_model(
+        model_uri=f"runs:/{run_id}/models",
+        name='best-model-final2-realone'
+    )
 
 
 if __name__ == '__main__':
--- 02-experiment-tracking/train.py
+++ 02-experiment-tracking/train.py
@@ -2,8 +2,17 @@ import os
 import pickle
 import click
 
+import mlflow.sklearn
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+
+import mlflow
+
+
+def configure_mlflow():
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    mlflow.set_experiment("nyc-taxi-experiment")
+    mlflow.set_tag("developer", "facundo")
 
 
 def load_pickle(filename: str):
@@ -19,6 +28,8 @@ def load_pickle(filename: str):
 )
 def run_train(data_path: str):
 
+    mlflow.sklearn.autolog()
+
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
@@ -28,6 +39,10 @@ def run_train(data_path: str):
 
     rmse = mean_squared_error(y_val, y_pred) ** 0.5
 
+    mlflow.log_metric("rmse", rmse)
+    mlflow.sklearn.log_model(rf)
+
 
 if __name__ == '__main__':
+    configure_mlflow()
     run_train()
